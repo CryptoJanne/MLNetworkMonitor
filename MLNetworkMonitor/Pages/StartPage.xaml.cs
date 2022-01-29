366c9897c001b4bca23124b0e83acd488ba145be
max <maxlundh91@protonmail.com>
2022-01-29 13:56:51 +0100

    Initial commit

---
+++ MLNetworkMonitor/Pages/StartPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MLNetworkMonitor.Pages
+{
+    public class StartPage : UserControl
+    {
+        public StartPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ MLNetworkMonitor/Views/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MLNetworkMonitor.Views
+{
+    public class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);            
+        }
+    }
+}
